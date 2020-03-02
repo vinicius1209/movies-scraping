facc9b0890dddc11627d91ba3aa31d128f2b965b
Vinicius <viniciusadrianomachado@gmail.com>
2020-03-02 16:30:51 -0300

    Ajustado redirecionament

--- app/movies/routes.py
+++ app/movies/routes.py
@@ -2,20 +2,21 @@ from app import app
 from . import movie_bp
 from .movie import TheMovieDB
 from .cache import Cache, CacheItem
-from flask import jsonify, render_template, abort, request
-
+from flask import render_template, request, redirect
 
 # The Movie API
 themovie = TheMovieDB('1f54bd990f1cdfb230adb312546d765d')
 # Cache pages
 cache = Cache(20, app.config['EXPIRE_MINUTE'])
 
+@movie_bp.route('/', methods=['GET'])
 @movie_bp.route('/upcoming', methods=['GET'])
 @movie_bp.route('/upcoming/', methods=['GET'])
 @movie_bp.route('/upcoming/<int:page>', methods=['GET'])
 def upcoming(page=1):
     
     if "search" in request.args:
+
         search_query = request.args.get('search', '')
         upcoming_list = themovie.get_upcoming_movies_by_search(1, search_query)
         return render_template('upcoming.html', 
